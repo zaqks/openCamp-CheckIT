352a07df71e3c5e5efb1e75b10cc9b09cf7f72bd
zaqks <imzakbro@gmail.com>
2024-02-16 00:29:55 +0100

    init

---
+++ db/dbLinker.py
@@ -0,0 +1,23 @@
+from .dbClasses.Customer import Customer
+from .dbClasses.Shop import Shop
+from .dbClasses.Orders import Orders, Payments
+from .dbClasses.Staff import Staff
+from .dbClasses.Stats import Stats
+from .dbClasses.Search import Search as SearchEngine
+from .dbClasses.DbFixer import DbFixer
+from .dbClasses.Essentials import reverseDict
+from .dbClasses.Translation import Translation
+
+# run db tests
+# DbFixer.run()
+
+
+
+"""
+# domain = "http://10.42.0.1:8000"
+# domain = "http://127.0.0.1:8000"
+# domain = "http://192.168.215.8:8000"
+# domain = "http://192.168.112.156:8000"
+"""
+
+
